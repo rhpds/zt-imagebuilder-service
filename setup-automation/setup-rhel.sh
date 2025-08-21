05236614cdfd0398478d6087a9f08e5d794852c1
Matthew Yee <myee@redhat.com>
2025-08-21 11:26:26 -0700

    Add guestfish documentation and image for filesystem listing; update RHEL setup script to copy image

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -13,9 +13,7 @@
 
 mkdir -p /mnt/image
 mount /dev/sr0 /mnt/image
-
-
-
-
-
+cp /mnt/image/composer_api_3f3c1824_647.qcow2 /root/composer_api.qcow2
+chmod 700 /root/composer_api.qcow2
+umount /mnt/image
 
