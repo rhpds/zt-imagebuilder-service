4896f3709c099191a96659d4d174b2e7feeaa29e
Matthew Yee <myee@redhat.com>
2025-08-21 11:41:44 -0700

    Update disk image references in documentation and setup script for consistency

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -13,7 +13,7 @@
 
 mkdir -p /mnt/image
 mount /dev/sr0 /mnt/image
-cp /mnt/image/composer_api_3f3c1824_647.qcow2 /root/composer_api.qcow2
-chmod 700 /root/composer_api.qcow2
+cp /mnt/image/composer_api_3f3c1824_647.qcow2 /root/composer_api_disk.qcow2
+chmod 700 /root/composer_api_disk.qcow2
 umount /mnt/image
 
