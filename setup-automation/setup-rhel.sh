50bc91cefd28f7bd0eba16fbae8e9285636bc866
Matthew Yee <myee@redhat.com>
2025-08-21 12:02:09 -0700

    Update disk image paths in documentation and setup script to ensure consistency and clarity

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# LOG=/root/setup-log
-# #LOG=/dev/null
-
-# subscription-manager config --rhsm.manage_repos=1 >> $LOG
-# subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force >> $LOG
-
-# dnf install -y libguestfs guestfs-tools >> $LOG
-# hostnamectl set-hostname rhel-$(uuidgen | cut -c 32-)
-# echo "export LIBGUESTFS_BACKEND=direct" >> /root/.bashrc
-# exec $SHELL
-
 mkdir -p /mnt/image
 mount /dev/sr0 /mnt/image
-cp /mnt/image/composer_api_3f3c1824_647.qcow2 /root/composer_api_disk.qcow2
-chmod 700 /root/composer_api_disk.qcow2
+cp /mnt/image/composer_api_3f3c1824_647.qcow2 /var/lib/libvirt/images/composer_api_disk.qcow2
+chmod 700 /var/lib/libvirt/images/composer_api_disk.qcow2
 umount /mnt/image
-
+echo "Welcome to the Image Builder Service Lab" > /root/motd
